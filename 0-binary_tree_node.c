b70c95338b32dd3fe20ba72f004d41cae6e64c9b
Alex Nduta <wasremoved@gmail.com>
2023-11-01 09:29:14 +0300

    task 0 fixing betty errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,23 +1,23 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- * binary_tree_node - creates a new node for our tree
- * @parent: pointer to the parent node
- * @value: the value to be stored in the node
- * Return: the pointer to the node
- */
+* binary_tree_node - creates a new node for our tree
+* @parent: pointer to the parent node
+* @value: the value to be stored in the node
+* Return: the pointer to the node
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = (binary_tree_t*) malloc(sizeof(binary_tree_t));
+binary_tree_t *node = (binary_tree_t*) malloc(sizeof(binary_tree_t));
 
-    if (node == NULL)
-        return(NULL);
+if (node == NULL)
+return(NULL);
 
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
-    return (node);
+node->n = value;
+node->parent = parent;
+node->left = NULL;
+node->right = NULL;
+return (node);
 }
 
--- binary_trees.h
+++ binary_trees.h
@@ -7,20 +7,20 @@
 #include <string.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to 
- * @right: Pointer to tyhe right child node
- */
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to 
+* @right: Pointer to tyhe right child node
+*/
 
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
