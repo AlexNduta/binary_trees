a239d223fb6434aa66a81cdb355986f11ded7bec
Alex Nduta <wasremoved@gmail.com>
2023-11-01 16:19:57 +0300

    task 4; debugging

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,13 +11,14 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 if (node == NULL)
 {
 return (0);
-} else if(node->left == NULL && node->right == NULL)
+}
+else if (node->left == NULL && node->right == NULL)
 {
 return (1);
 }
-    else
+else
 {
-    return (0);
+return (0);
 }
 }
 
