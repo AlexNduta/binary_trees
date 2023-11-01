9284c4a6363cd8bb9b19135bd7ec24e9f650a86d
Alex Nduta <wasremoved@gmail.com>
2023-11-01 09:26:31 +0300

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,9 @@
-#ifndef BINARY_H
-#define BINARY_H
+#ifndef BINARY_TREES_H
+#define BINARY_TREE_H
 
 #include <stddef.h>
 #include <stdio.h>
-#include <sdtlib.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -21,14 +21,14 @@ struct binary_tree_s
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-}
+};
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /* tester function */
-void binary_tree_print(const binary_tree_t *);
+void binary_tree_print(const binary_tree_t * tree);
 
 /*project */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
